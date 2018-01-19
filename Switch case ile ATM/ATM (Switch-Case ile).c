633fadd8fea92a3ae79900e643d9d18dd8c8c051
terzioglumtn <terzioglumtn@hotmail.com>
2018-01-19 14:16:45 +0300

    Switch case ile ATM

---
+++ Switch case ile ATM/ATM (Switch-Case ile).c
@@ -0,0 +1,49 @@
+//switch case örneði.ATM...
+#include <stdio.h>
+void main()
+{
+int islem,bakiye=1000,tutar;
+printf("islemler\n1=Para Cekme\n2=Para Yatirma\n3=Havale Yapma\n4=Bakiye Sorgulama\n5=Kart iade\n\n\n");
+printf("islem seciniz=");
+scanf("%d",&islem);
+switch(islem)
+			{
+	case 1:
+	    printf("Bakiyeniz=%d\n",bakiye);
+	    printf("Cekmek istediginiz tutari giriniz=");
+	    scanf("%d",&tutar);
+	    if(tutar>1000)
+		{
+		printf("Yetersiz miktar girdiniz!\nYeniden giriniz=");
+	    scanf("%d",&tutar);
+		}
+	    bakiye=bakiye-tutar;
+	    printf("Yeni Bakiyeniz=%d",bakiye);
+	    break;
+	case 2:
+	    printf("Bakiyeniz=%d\n",bakiye);
+	    printf("Yatirmak istediginiz tutari giriniz=");
+	    scanf("%d",&tutar);
+	    bakiye=bakiye+tutar;
+	    printf("Yeni Bakiyeniz=%d",bakiye);
+	    break;
+	case 3:
+	    printf("Bakiyeniz=%d\n",bakiye);
+	    printf("Havale etmek istediginiz tutari giriniz=");
+	    scanf("%d",&tutar);
+	    if(tutar>1000)
+		{
+	    printf("Yetersiz miktar girdiniz!\nYeniden giriniz=");
+	    scanf("%d",&tutar);
+     	}
+	    bakiye=bakiye-tutar;
+	    printf("Yeni Bakiyeniz=%d",bakiye);
+	    break;	
+	case 4:
+	    printf("Bakiyeniz=%d\n",bakiye);
+	    break;	
+	case 5:
+	    printf("Kartinizi almayi unutmayin!");
+	    break;			
+	}
+}
